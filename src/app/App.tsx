c4e9467dee6d6ef83adf19603dae7892727ed982
Siva Guhan S <sivaguhan@Sivas-MacBook-Pro.local>
2025-04-18 13:44:12 +0530

    added font utility classes

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <div>Hii, I'm Siva</div>
+    </>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { AppProvider } from './store/AppContext'
 import './styles/index.scss'
-import App from './App'
+import App from './app/App'
 
 createRoot(document.getElementById('root')!).render(
   <AppProvider>

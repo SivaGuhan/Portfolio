7d837f41b7a38d057edf18b25678a180030cca81
Siva Guhan S <sivaguhan@Sivas-MacBook-Pro.local>
2025-04-19 19:11:01 +0530

    added navbar

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -1,25 +1,38 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { MENU_OPTIONS } from "./constants";
 import { MenuOption } from "./types";
+import { RxHamburgerMenu, RxAvatar } from "react-icons/rx";
 
 const NavBar: FC = () => {
+    const [isNavBarOpen, setIsNavBarOpen] = useState(false);
 
+    const onClickHamburger = () => {
+        setIsNavBarOpen((prev) => !prev);
+    }
 
     return (
-        <nav className="nav-container">
-            <div>
-                <p>Portfolio</p>
-            </div>
-            <ul>
-                {MENU_OPTIONS.map((option: MenuOption) => (
-                    <li
-                        key={option.value}
-                    >
-                        {option.label}
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <div className="nav-container">
+            <nav className="nav-inner-container">
+                <div className="avatar">
+                    <RxAvatar />
+                </div>
+                <div className={`nav-list-wrapper-container ${isNavBarOpen ? "open" : "closed"}`}>
+                    <ul className={`nav-list-wrapper`}>
+                        {MENU_OPTIONS.map((option: MenuOption) => (
+                            <li
+                                key={option.value}
+                                className="nav-list-item"
+                            >
+                                {option.label}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="hamburger" onClick={onClickHamburger}>
+                    <RxHamburgerMenu />
+                </div>
+            </nav>
+        </div>
     )
 }
 
